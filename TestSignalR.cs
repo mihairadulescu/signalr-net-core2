e7f7aae0c1044d0590f4888b6962f9f8b9c25c71
mihai.radulescu <mihai.radulescu@nerdscomputing.com>
2017-10-20 13:22:33 +0300

    initial commit

---
+++ TestSignalR.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TESTING
+{
+    public class TestSignalR : Controller
+    {
+        private readonly IHubContext<NotificationHub> _notificationHub;
+
+        public TestSignalR(IHubContext<NotificationHub> notificationHub)
+        {
+            _notificationHub = notificationHub;
+        }
+        // GET: /<controller>/
+        public string Index()
+        {
+            _notificationHub.Clients.All.InvokeAsync("notify", "hey dude whats up");
+            return "Mihai Testing";
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Write your Javascript code.
+
+$(function() {
+    var connection = new signalR.HubConnection('/notifications');
+    connection.on("notify", function(message) {
+        $("#content-notification").text(message);
+    });
+    connection.start();
+});
